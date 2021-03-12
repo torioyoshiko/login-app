765c2b2859a71102855dde3f563fc3abcca3fb32
Torio Yoshiko <torio.yoshiko@gmail.com>
2021-03-12 21:45:06 +0100

    Updates

---
+++ precache-manifest.2cdee78a9c034cc37eb2c0efac89ee7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e8f30bfbcbb145c8af7695d88f1a6e",
+    "url": "/login-app/index.html"
+  },
+  {
+    "revision": "36e2f1650cd6e77f0aeb",
+    "url": "/login-app/static/css/main.03247a6f.chunk.css"
+  },
+  {
+    "revision": "eee2cfbb25c4b49f4fba",
+    "url": "/login-app/static/js/2.c30556e5.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/login-app/static/js/2.c30556e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e2f1650cd6e77f0aeb",
+    "url": "/login-app/static/js/main.f127c352.chunk.js"
+  },
+  {
+    "revision": "420cc025fa2739a1d344",
+    "url": "/login-app/static/js/runtime-main.2f55f01a.js"
+  }
+]);
